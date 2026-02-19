9ccc4c5fe86c1d46d75e3a851f12ada88689a1b4
Frank Mwase <frankmwase925@gmail.com>
2026-02-19 15:04:31 +0200

    Update: Updated theme toggle feature and added zero validation

--- script.js
+++ script.js
@@ -3,27 +3,50 @@ const checkBtn = document.getElementById("checkBtn");
 const result = document.getElementById("result");
 const themeToggle = document.getElementById("themeToggle")
 
-checkBtn.addEventListener("click", () => {
-    const num = parseInt(numberInput.value);
+function applyTheme(theme) {
+    if(theme === "light") {
+        document.body.classList.add("light-mode");
+        themeToggle.checked = true;
+    } else {
+        document.body.classList.remove("light-mode");
+        themeToggle.checked = false;
+    }
 
-    if (isNaN(num)) {
-        result.textContent = "Please enter a valid number";
+
+}
+// Load saved theme
+ const savedTheme = localStorage.getItem("theme") || "dark";
+ applyTheme(savedTheme);
+
+ themeToggle.addEventListener("change", () => {
+    const newTheme = themeToggle.checked ? "light" : "dark";
+    applyTheme(newTheme);
+    localStorage.setItem("theme",newTheme);
+ })
+
+ // Even/ Odd Logic
+checkBtn.addEventListener("click", () => {
+    const num = Number(numberInput.value);
+
+    if(numberInput.value.trim() === "" || isNaN(num)) {
+        result.textContent = "Please enter a number valid number";
+        result.style.color = "red";
+        return;
+    }
+
+    if(num === 0) {
+        result.textContent = "Zero is neither Odd nor Even in this checker";
         result.style.color = "orange";
         return;
     }
 
     if (num % 2 === 0) {
-        result.textContent = `${num} is Even `;
+        result.textContent = `${num} is Even`;
         result.style.color = "lightgreen";
     } else {
         result.textContent = `${num} is Odd`;
         result.style.color = "skyblue";
     }
-}); 
+    }); 
 
-themeToggle.addEventListener("click", () => {
-    document.body.classList.toggle("light-mode");
-
-    const isLight = document.body.classList.contains("light-mode");
-    themeToggle.textContent = isLight ? "☀️": "🌙";
-})
+ 
